4f36442d22d0293f7861f06ff854e6e13d536721
Chetas Patel <94209839+MrRogerTag1345@users.noreply.github.com>
2024-07-07 16:39:17 -0400

    Made Some Changes

--- my-website/src/components/Header.js
+++ my-website/src/components/Header.js
@@ -1,19 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Header.css';
-import AnimatedLogo from './AnimatedLogo';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Header = () => {
+    const [darkMode, setDarkMode] = useState(true);
+
+    const toggleTheme = () => {
+        setDarkMode(!darkMode);
+        document.body.classList.toggle('dark-mode', !darkMode);
+    };
 
     return (
-        <header className='header' >
-            {/*
-            <button className='SidebarButton'>
-                 Implement sidebarbutton 
-            </button>
-            */}
+        <header className='header'>
             <span className='TitleSpan'>
                 <h1>Welcome, this is <span className="MyName">Chetas</span>!</h1>
             </span>
+
+            <div className="social-icons">
+                {/*
+                <button id="themeToggle" className="social-icon" onClick={toggleTheme}>
+                    <i className={darkMode ? "fas fa-sun" : "fas fa-moon"}></i>
+                </button>
+                */}
+                <a href="mailto:Chetaspatel1345@gmail.com" className="social-icon">
+                    <i className="fas fa-envelope"></i>
+                </a>
+                <a href="https://github.com/MrRogerTag1345" target="_blank" rel="noopener noreferrer" className="social-icon">
+                    <i className="fab fa-github"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/chetas-patel-ba9379165/" target="_blank" rel="noopener noreferrer" className="social-icon">
+                    <i className="fab fa-linkedin"></i>
+                </a>
+            </div>
         </header>
     );
 };
--- my-website/src/components/Sidebar.js
+++ my-website/src/components/Sidebar.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import './Sidebar.css';
+import MyPhoto from '../images/LogoSC.png'; // Update the path to your image
+import './Sidebar.css'; // Import the CSS file for additional styling
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            <div className="Sidebar-TopElement">
-                <h2 style={{ textAlign: 'left' }}>Options</h2>
-            </div>
-            
-            <div className='ButtonElement'>
-                <button className="SidebarButton">
-                    <h2>Home</h2>
-                </button>
-                <button className="SidebarButton">
-                    <h2>Experience</h2>
-                </button>
-                <button className="SidebarButton">
-                    <h2>Projects</h2>
-                </button>
-                <button className="SidebarButton">
-                    <h2>Other</h2>
-                </button>
-            </div>
+  return (
+    <div className="sidebar">
+      <div className="Sidebar-TopElement">
+        <div className='SidebarImageComponent'>
+          <img className="Image" src={MyPhoto} alt="My Photo" />
         </div>
-    );
+        <h2>Chetas Patel</h2>
+      </div>
+      <div className='ButtonElement'>
+        <button className="SidebarButton">
+          <h2>Home</h2>
+        </button>
+        <button className="SidebarButton">
+          <h2>Experience</h2>
+        </button>
+        <button className="SidebarButton">
+          <h2>Projects</h2>
+        </button>
+        <button className="SidebarButton">
+          <h2>Other</h2>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
