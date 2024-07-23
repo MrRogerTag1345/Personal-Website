4082976886813abddec8065adac95f2d2b371505
Chetas Patel <94209839+MrRogerTag1345@users.noreply.github.com>
2024-07-23 12:49:19 -0400

    Added Python Projects to the Page.

--- my-website/src/pages/Projects.js
+++ my-website/src/pages/Projects.js
@@ -5,23 +5,34 @@ import './Projects.css'; // Import the CSS file
 const Projects = () => {
   const weblinks = [
     {
-      text: 'Fuel Economy Logger',
-      url: '/Fuel-Economy-Logger/index.html',
-      description: 'This webpage is my introduction to creating Webpages using HTML and CSS.',
+      text: 'Fuel Economy Calculator',
+      url: '',
+      description: "This webpage is my introduction to creating Webpages using HTML and CSS. This website was create to track fuel expenses per communite to school and back. Since I communite to school(waterloo) during the weekdays, I have driven thousands' of kilometres. In order to track my monthly expenses, and to inform me about how much I am spending on fuel. This website give's me a break down of all the information I would need.",
       additionalLinks: [
-        { text: 'More Info', url: '/project-1/info' }, // Will have to add an info page.
+        { text: 'More Info', url: '/Fuel-Economy-Calculator/info' }, // Will have to add an info page.
         { text: 'GitHub', url: 'https://github.com/MrRogerTag1345/Fuel-Economy-Logger', external: true }
       ]
     },
     {
       text: 'Student-Financial-App',
-      url: '/Fuel-Economy-Logger/index.html',
-      description: 'This webpage is my introduction to creating Webpages using HTML and CSS.',
+      url: '',
+      description: 'Within this project I took on the role of Project Manager. This project is a web application that is intended to help students manage there finances.It offers tools to track the budget/ balance to setting financial goals.',
       additionalLinks: [
-        { text: 'More Info', url: '/project-2/info' }, // Will have to add an info page.
+        { text: 'More Info', url: '/Student-Financial-App/info' }, // Will have to add an info page.
         { text: 'GitHub', url: 'https://github.com/lorandkis/Student-Financial-App', external: true }
       ]
     }
+  ];
+
+  const pythonlinks = [
+    {
+      text: 'Discord Stock Bot',
+      url: '/Discord-Stock-Bot/info',
+      description: "This Discord Bot is a sophisticated tool designed to retrieve the URL of a specified stock effortlessly and dynamically incorporate it into a link.",
+      additionalLinks: [
+        { text: 'GitHub', url: 'https://github.com/MrRogerTag1345/Fuel-Economy-Logger', external: true }
+      ]
+    },
   ];
 
   return (
@@ -30,6 +41,9 @@ const Projects = () => {
         <h1>Projects Page</h1>
         <h2>Web Development</h2>
         <GridLinks links={weblinks} />
+
+        <h2>Python Project</h2>
+        <GridLinks links={pythonlinks} />
       </main>
     </div>
   );
